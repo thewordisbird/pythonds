2a6d303670522b37d7c116d036f53e86aa3c9406
JustinHBird <justin@thewordisbird.dev>
2019-10-11 13:45:46 -0700

    recursive solution for all_substrings

--- Recursion/pset/practice/all_substrings.py
+++ Recursion/pset/practice/all_substrings.py
@@ -1,17 +1,34 @@
-# Find all substrings of a given string. For example:
-#   given 'abc' return 'a', 'b', 'c', 'ab', 'bc', 'ac', 'abc'
+# Find all substrings of a given string. 
+# For example:
+# Given 'abc' return 'a', 'b', 'c', 'ab', 'bc', 'ac', 'abc'
 
-def substings_in_string(string, cache=[]):
-    if len(string) == 0:
-        return
-    if string not in cache:
-        cache.append(string)
+# For problems where all end nodes need to be returned or stored, The action must happen in the base case
+# otherwise recurse to the next level with and without the added item
 
-    substings_in_string(string[1::], cache)
-    substings_in_string(string[0:-1], cache)
+#   0       1       2       3       4       5       6       7
+#               start string ='abc'                 
+#               /                \              Keep or remove 'a'
+#           'abc'                 'bc'
+#         /     \                /    \         Keep or remove 'b'
+#    'abc'       'ac'        'bc'      'c' 
+#   /     \     /    \     /     \    /   \     Keep or remove 'c'
+# 'abc'  'ab'  'ac'  'a'  'bc'  'b'  'c'  ''                      
+# 
+# When the bottome of the tree is reached that is the base case. 
+# At this point perform action. In this case add to the sub array
+     
+def substrings(s, subs=set(), i=0):
+    '''Recursive function to find all substrings in a string'''
+    if i == len(s):
+        subs.add(s)
+    else:
+        # recurse down including s[i]
+        substrings(s, subs, i+1)
+        # recurse down not including s[i]
+        substrings(s[0:i] + s[i+1::], subs, i)
+    return subs
     
-    return cache
 
 if __name__ =='__main__':
-    s = 'abcd'
-    print(substings_in_string(s))
+    s = 'aaa'
+    print(substrings(s), len(substrings(s)))
