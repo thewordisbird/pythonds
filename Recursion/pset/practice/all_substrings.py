45ecfeaa398a6579b67c7bb5b47f5a4f83ac4cfb
JustinHBird <justin@thewordisbird.dev>
2019-10-11 15:28:52 -0700

    notes on DP solution. DP not solved

--- Recursion/pset/practice/all_substrings.py
+++ Recursion/pset/practice/all_substrings.py
@@ -16,6 +16,12 @@
 # 
 # When the bottome of the tree is reached that is the base case. 
 # At this point perform action. In this case add to the sub array
+
+# Time complexity O(2^n) space complexity O(n)
+
+# There has to be a dynanic programing solution to bring time complexity to O(n^2)
+# I can see it in the tree, but can't figure out how to code the memoization
+
      
 def substrings(s, subs=set(), i=0):
     '''Recursive function to find all substrings in a string'''
@@ -27,8 +33,11 @@ def substrings(s, subs=set(), i=0):
         # recurse down not including s[i]
         substrings(s[0:i] + s[i+1::], subs, i)
     return subs
-    
+
+
+
+
 
 if __name__ =='__main__':
-    s = 'abc'
+    s = 'abcd'
     print(substrings(s), len(substrings(s)))
