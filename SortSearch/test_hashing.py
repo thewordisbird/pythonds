51b026d2d6a7e36be6e470075c4a90128e557e24
Justin Bird <justin@thewordisbird.dev>
2019-10-18 09:29:17 -0700

    Testing Map ds

--- SortSearch/test_hashing.py
+++ SortSearch/test_hashing.py
@@ -1,5 +1,5 @@
 import pytest
-from hashing import remainder_hash, folding_hash, mid_square_hash
+from hashing import remainder_hash, folding_hash, mid_square_hash, Map
 # Tests for hashing.py
 
 @pytest.mark.parametrize('size, item, result',
@@ -41,3 +41,26 @@ def test_folding_hash(size, item, group, result):
 def test_mid_square_hash(size, item, result):
     assert mid_square_hash(size, item) == result
 
+@pytest.fixture
+def map_fixture():
+    size = 7
+    return Map(7)
+
+def test_Map_construction(map_fixture):
+    '''Test that class constructor works'''
+    testMap = map_fixture
+    assert testMap.size == 7
+    assert testMap.slots[1] == None
+    assert testMap.data[5] == None
+    assert testMap.items == 0
+
+@pytest.mark.parametrize('item, result',
+                        [
+                            (10, 3),
+                            (13, 6),
+                            (11, 4),
+                            (0, 0)
+                        ])
+def test_Map_hash(map_fixture, item, result):
+    testMap = map_fixture
+    assert testMap.remainder_hash(item) == result
