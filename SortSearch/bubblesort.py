35cdddb9c5b09673fa3497339e0eb2629e3d1506
Justin Bird <justin@thewordisbird.dev>
2019-10-21 03:11:49 -0700

    added short bubble to bubble sort

--- SortSearch/bubblesort.py
+++ SortSearch/bubblesort.py
@@ -6,21 +6,30 @@ def bubble_sort_r(nums, n=None):
     if n == 1:
         return nums
     else:
+        exchange = False
         for i in range(n):
             if nums[i] > nums[i + 1]:
+                exchange = True
                 nums[i], nums[i + 1] = nums[i + 1], nums[i]
-        return bubble_sort_r(nums, n - 1)
+        if exchange == False:
+            return nums
+        else:
+            return bubble_sort_r(nums, n - 1)
 
 
 def bubble_sort_i(nums):
     for i in range(len(nums) - 1, 0 , -1):
+        exchange = False
         for j in range(i):
             if nums[j] > nums[j + 1]:
+                exchange = True
                 nums[j], nums[j + 1] = nums[j + 1], nums[j]
+        if exchange == False:
+            return nums
 
     return nums
 
 
 if __name__ == '__main__':
-    print(bubble_sort_r([2, 5, 1, 6, 3]))
-    print(bubble_sort_i([2, 5, 1, 6, 3]))
+    print(bubble_sort_r([2, 1, 3, 4]))
+    print(bubble_sort_i([1, 2, 4, 3]))
