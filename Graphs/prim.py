58ada6ebf308b7beaf8d5bad04d2a88af4f5344a
JustinHBird <justin@thewordisbird.dev>
2019-12-10 14:38:19 -0800

    Prim's algortihm implementation

--- Graphs/prim.py
+++ Graphs/prim.py
@@ -6,17 +6,18 @@
 #   The lowest weight at each iteration
 
 # Steps:
-# 1) Create a Min Heap of size V where V is the number of vertices in the given graph. 
-#       Every node of min heap contains vertex number and key value of the vertex.
-# 2) Initialize Min Heap with first vertex as root (the key value assigned to first vertex is 0). 
-#       The key value assigned to all other vertices is INF (infinite).
-# 3) While Min Heap is not empty, do following
-#   a) Extract the min value node from Min Heap. Let the extracted vertex be u.
-#   b) For every adjacent vertex v of u, check if v is in Min Heap (not yet included in MST). 
-#       If v is in Min Heap and its key value is more than weight of u-v, then update the key value of v as weight of u-v.
+# 1. Initialization
+#   a. Initialize MinHeap and populate with all nodes setting distance to infinity
+#   b. Initialize parent dict to maintain map of child to parent. This will be used to create
+#       the MST
+# 2. decrease_key() for the starting node, setting it's value to 0. This allows it to be the first
+#       node extracted once in the while loop
+# 3. Enter while loop
+#   a. extract_min to get the min distance node
+#   b. iterate over neighbors and if they are in the MinHeap, decrease_key if the edge distance is 
+#       less than the stored distance in the MinHeap and update parent in the parent dict
+# 4. Return the parent set.
 
-from collection import defaultdict
-import sys
 
 class HeapNode:
 
@@ -48,20 +49,24 @@ class MinHeap:
         return self.heap_list[self.get_right_child_index(index)]
 
     def has_left_child(self, index):
-        return self.get_left_child_index() < self.size
+        return self.get_left_child_index(index) < self.size - 1
 
     def has_right_child(self, index):
-        return self.get_right_child_index() < self.size
+        return self.get_right_child_index(index) < self.size - 1
+
+    def get_parent_index(self, index):
+        return (index - 1) // 2
 
     def get_parent(self, index):
-        return (index - 1) // 2
+        return self.heap_list[self.get_parent_index(index)]
 
     def has_parent(self, index):
         return index != 0
 
     def get_smallest_child_index(self, index):
-        if self.has_right_child(index) and \ 
-            self.get_right_child(index).value < self.get_left_child(index).value
+        # Find the smalles child given a node index. This is only called from a method that checks
+        # that the node has a left child to maintain validity
+        if self.has_right_child(index) and self.get_right_child(index).value < self.get_left_child(index).value:
             return self.get_right_child_index(index)
         return self.get_left_child_index(index)
 
@@ -69,15 +74,21 @@ class MinHeap:
         # Add all nodes to the priority queue with a path distance
         # set to infinity. 
         for node in graph:
-            node = HeapNode(node, flaot('inf'))
+            node = HeapNode(node, float('inf'))
             self.heap_list.append(node)
             self.node_map[node.key] = self.size
             self.size += 1
 
-    def swap(self, node_a, node_b):
-        self.heap_list[node_a], self.heap_list[node_b] = self.heap_list[node_b], self.heap_list[node_a]
+    def swap(self, index_a, index_b):
+        # Swap nodes and update their location in the node map
+        a = self.heap_list[index_a].key
+        b = self.heap_list[index_b].key
+        self.heap_list[index_a], self.heap_list[index_b] = self.heap_list[index_b], self.heap_list[index_a]
+        self.node_map[a] = index_b
+        self.node_map[b] = index_a
 
     def heapify_down(self, index=0):
+        # Settle node into position from top dowin in the heap
         while self.has_left_child(index):
             smallest_child_index = self.get_smallest_child_index(index)
             if self.heap_list[index].value > self.heap_list[smallest_child_index].value:
@@ -87,6 +98,7 @@ class MinHeap:
                 break
 
     def heapify_up(self, index=None):
+        # Settle node into position from bottom up in the heap
         if index == None:
             index = len(self.heap_list) - 1
 
@@ -95,26 +107,70 @@ class MinHeap:
             index = self.get_parent_index(index)
 
     def extract_min(self):
+        # Return the minimum valued node in the heap
         if len(self.heap_list) > 1:      
             root = self.heap_list[0]
-            self.node_map[root.key] = None
+            del self.node_map[root.key]
             
             tail = self.heap_list.pop()
+            self.size -= 1
             self.heap_list[0] = tail
             
             self.node_map[tail.key] = 0
             self.heapify_down()
         else:
-            item = self.heap_list.pop()
-        return item
+            root = self.heap_list.pop()
+        return root
 
     def decrease_key(self, key, value):
+        # Update the node distance, value, of a node with key, key in the heap
         index = self.node_map[key]
         self.heap_list[index].value = value
         self.heapify_up(index)
+
+    def get_weight(self, key):
+        # Return the value of a node in the heap
+        index = self.node_map[key]
+        return self.heap_list[index].value
     
 def prim(graph, start):
+    # Initialization. Create heap instance, add graph to heap set starting node and initialize parent
+    # dictionary
     pq = MinHeap()
     pq.build_heap(graph)
     pq.decrease_key(start, 0)
     parent = {}
+    parent[start] = None
+
+    # Loop until MinHeap priority queue is empty
+    while not pq.is_empty():
+        current_node = pq.extract_min()
+        print(pq.node_map)
+        # Iterate over current node's neighbors and update wieghts in pq for nodes in pq who's 
+        # values are less than the stored value
+        for neighbor in graph[current_node.key]:
+            if neighbor in pq.node_map:
+                neighbor_weight = graph[current_node.key][neighbor]
+                if neighbor_weight < pq.get_weight(neighbor):
+                    pq.decrease_key(neighbor, neighbor_weight)
+                    parent[neighbor] = current_node.key
+
+    return parent
+
+if __name__ == '__main__':
+    
+
+    graph = {
+                'a': {'f': 7, 'h': 15, 'b': 10}, 'b': {'a': 10, 'c': 3, 'd': 8},
+                'c': {'b': 3, 'd': 14}, 'd': {'b': 8, 'c': 14, 'e': 6, 'f': 5},
+                'e': {'d': 6, 'f': 12}, 'f': {'a': 7, 'd': 5, 'e': 12, 'g': 9},
+                'g': {'f': 9}, 'h': {'a': 15}
+            }  
+
+    graph = {
+                'u': {'v': 2, 'x': 1, 'w': 5}, 'v': {'u': 2, 'x': 2, 'w': 3},
+                'x': {'u': 1, 'v': 2, 'w': 3, 'y': 1}, 'w': {'v': 3, 'u': 5, 'x': 3, 'y': 1, 'z': 5},
+                'y': {'x': 1, 'w': 1, 'z': 1}, 'z': {'w': 5, 'y': 1}
+            }    
+
+    print(prim(graph, 'y'))
